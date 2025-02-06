1d2ed177de0c54e9e5973a03031a23f56e7acd24
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-02-06 22:51:12 +0100

    fix(create-technology): fix mutation resolver

--- src/server/src/graph/mutations/technologies/create-technology.mutation.ts
+++ src/server/src/graph/mutations/technologies/create-technology.mutation.ts
@@ -1,9 +1,9 @@
-import { DbContext } from '../../../data/db-context';
-import { Technology } from '../../../data/model/technology.type';
+import { Technology } from '../../../data/model';
+import { TechnologyService } from '../../../data/service';
 import { CreateTechnologyInput } from '../inputs/create-technology.input';
 
-export async function createTechnology(_: any, { input }: { input: CreateTechnologyInput }, { dbContext }: { dbContext: DbContext }): Promise<Technology | null> {
-  return await dbContext.addTechnology({
+export async function createTechnology(_: any, { input }: { input: CreateTechnologyInput }, { technologyService }: { technologyService: TechnologyService }): Promise<Technology | undefined> {
+  return await technologyService.addTechnology({
     ...input,
     created: new Date(),
   })
