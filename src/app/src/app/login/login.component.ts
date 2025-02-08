60ba00c9968ac437a1bedec71ee17efc0c46f0b4
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-02-08 20:35:21 +0100

    feat(login): add basic login component

---
+++ src/app/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  async onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log(`email: ${email}, password: ${password}`);
+    }
+  }
+}
