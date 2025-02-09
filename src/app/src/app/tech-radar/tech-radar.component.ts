1e093ecbaefa06c321dc974c3755c2c1206d23b4
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-02-09 14:25:32 +0100

    feat(tech-radar): add basic technology pie chart

--- src/app/src/app/tech-radar/tech-radar.component.ts
+++ src/app/src/app/tech-radar/tech-radar.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
-import { first } from 'rxjs/operators';
+
+import * as echarts from 'echarts/core';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+import { PieChart } from 'echarts/charts';
+import { CanvasRenderer } from 'echarts/renderers';
+echarts.use([CanvasRenderer, PieChart]);
 
 import { HeaderComponent } from '../shared/header/header.component';
 import { LogoutComponent } from '../shared/logout/logout.component';
-import { TechnologiesService } from '../services/technologies/technologies.service';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-tech-radar',
-  imports: [HeaderComponent, LogoutComponent],
+  imports: [HeaderComponent, LogoutComponent, NgxEchartsDirective, CommonModule],
   templateUrl: './tech-radar.component.html',
-  styleUrl: './tech-radar.component.scss'
+  styleUrl: './tech-radar.component.scss',
+  providers: [provideEchartsCore({ echarts })],
 })
-export class TechRadarComponent implements OnInit {
+export class TechRadarComponent {
 
-  private readonly technologiesService = inject(TechnologiesService);
+  private readonly authService = inject(AuthService);
 
-  technologies$ = this.technologiesService.getTechnologies();
+  private readonly router = inject(Router);
 
-  ngOnInit(): void {
-    this.technologies$.pipe(first()).subscribe(console.log)
-  }
+  me$ = this.authService.getMe();
+
+  chartOptions = {
+    color: ['#dedede', '#d4d4d4', '#e6e6e6'],
+    series: [
+      {
+        name: 'Tech-Radar',
+        type: 'pie',
+        radius: '70%',
+        data: [
+          { value: 20, name: 'Techniques' },
+          { value: 20, name: 'Tools' },
+          { value: 20, name: 'Platforms' },
+          { value: 20, name: 'Languages' },
+          { value: 20, name: 'Frameworks' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
 }
