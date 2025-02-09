431f7670bb084ad939800a791415e1dbc02aabd2
Kevin Wüstner <kevin.wuestner@xappido.com>
2025-02-09 16:05:01 +0100

    feat(header): routing to header

--- src/app/src/app/category/category.component.ts
+++ src/app/src/app/category/category.component.ts
@@ -1,11 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from '../shared/header/header.component';
+import { LogoutComponent } from '../shared/logout/logout.component';
+import { TechnologiesService } from '../services/technologies/technologies.service';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    LogoutComponent,
+    CommonModule,
+  ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
 export class CategoryComponent {
+    private readonly technologyService = inject(TechnologiesService);
+
     @Input() category?: string;
 }
